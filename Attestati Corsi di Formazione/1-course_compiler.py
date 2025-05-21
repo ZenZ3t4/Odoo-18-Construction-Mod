0c9cf27da663c95563f6ff38f01c50d75ff19267
ZenZ3t4 <145878511+ZenZ3t4@users.noreply.github.com>
2025-05-21 13:43:53 +0200

    aggiunta filigrana + firma

--- Attestati Corsi di Formazione/1-course_compiler.py
+++ Attestati Corsi di Formazione/1-course_compiler.py
@@ -9,15 +9,14 @@ import os
 # FUNZIONI UTILIZZATE NEL CODICE
 # -----------------------------------------
 def stampa_dati_azienda(pdf, azienda, settore_ateco, ente_formativo, ente_certificatore):
-    pdf.ln(5)
     # BLOCCO DATI AZIENDA
     if len(settore_ateco) > 80:
         settore_ateco = settore_ateco[:80] + '...'
-    
+
     # Dati dinamici azienda
-    pdf.set_font("Helvetica", "I", size=10)
+    pdf.set_font("Helvetica", "I", size=11)
     #azienda_row_1 = f"{azienda} - Settore ateco: {settore_ateco}"
-    azienda_row_1 = f"{azienda}"
+    azienda_row_1 = f"collaboratore dell'azienda {azienda}"
     pdf.cell(200, 5, txt=azienda_row_1, align = 'C', ln=True)
     #--------------------------------------------------------------------------------
     
@@ -28,7 +27,7 @@ def blocco_dati_utente(pdf, data_nascita, luogo_nascita, cognome, nome, codice_f
     pdf.set_margins(10, 10, 10)
     testo_rilasciato_a = "il presente attestato viene conferito a"
     pdf.cell(200, 10, txt=testo_rilasciato_a, align = 'C', ln=True)
-    pdf.ln(10)
+    pdf.ln(7)
     # blocco dati dinamici
     # dati anagrafici del candidato
     nome_cognome = nome + ' ' + cognome
@@ -45,29 +44,52 @@ def blocco_dati_utente(pdf, data_nascita, luogo_nascita, cognome, nome, codice_f
     # stampa data_nascita,luogo,ecc...
     pdf.set_margins(10, 10, 10)
     pdf.set_font("Helvetica", "I", size=12)
-    pdf.set_text_color(255,250,250)
+    pdf.set_text_color(36, 35, 35)
     pdf.cell(200, 5, txt=dati_utente, align = 'C', ln=True)
     pdf.ln(5)  # Aggiunge una nuova linea
-
-def stampa_intestazione_pdf(pdf, image_path, header_text):
-    # immagine ANPAL/UE in intestazione
-    pdf.set_margins(10, 10, 10)
-    pdf.image(image_path, x=10, y=8, w=190)  # Modifica i valori di x, y, e w secondo necessità
-    pdf.ln(12)  # Spazio dopo l'immagine
+    
     # Stampa dettaglio azienda
     azienda = info['azienda'] + " - P.IVA: " + info['partita_iva']
     settore_ateco = info['settore_ateco']
     ente_formativo = info['ente_formativo']
     ente_certificatore = info['ente_certificatore']
     stampa_dati_azienda(pdf, azienda, settore_ateco, ente_formativo, ente_certificatore)
+
+def disegna_linea(pdf):
+    # DISEGNA LINEA ORIZZONTALE
+    y_pos = pdf.get_y()
+    # Impostazioni linea
+    w = pdf.w
+    line_length = (3/4) * w
+    line_thickness = 0.5
+    x_start = (w - line_length) / 2
+
+    pdf.set_draw_color(36, 35, 35)
+    pdf.set_draw_color(200, 200, 200)
+    pdf.set_line_width(line_thickness)
+
+    # Disegna la linea qualche mm sotto il testo (ad esempio 2 mm)
+    pdf.line(x_start, y_pos + 2, x_start + line_length, y_pos + 2)
+    pdf.ln(7)  # Spazio dopo header
+    
+def stampa_intestazione_pdf(pdf, image_path, header_text):
+    # immagine ANPAL/UE in intestazione
+    pdf.set_margins(10, 10, 10)
+    pdf.ln(5)
+    pdf.image(image_path, x=10, y=8, w=190)  # Modifica i valori di x, y, e w secondo necessità    
     pdf.ln(20)  # Spazio dopo l'immagine
+    disegna_linea(pdf)
+    pdf.ln(5)  # Spazio dopo header
     # INTESTAZIONE DEL DOCUMENTO - SUBITO SOTTO IMMAGINE HEADER
     pdf.set_text_color(235, 9, 16)
     pdf.set_font("Helvetica", "B", size=36)
     pdf.cell(193, 7, txt=header_text, align = 'C', ln=True)
-    pdf.ln(10)  # Spazio dopo header
-    pdf.set_text_color(255,250,250)
-
+    pdf.ln(5)  # Spazio dopo header
+    pdf.set_text_color(36, 35, 35)
+    disegna_linea(pdf)
+    pdf.ln(3)  # Spazio dopo header
+    
+    
 
 # Funzione che stampa "per la partecipazione al corso di formazione generale e specifica" ed il relativo corso passato come parametro
 def stampa_denominazione_percorso_sviluppo(pdf, nome_corso_da_stampare):
@@ -75,20 +97,29 @@ def stampa_denominazione_percorso_sviluppo(pdf, nome_corso_da_stampare):
     pdf.set_font("Helvetica", "BI", size=14)
     testo_header = "per la partecipazione al corso di formazione generale e specifica"
     pdf.cell(200, 7, txt=testo_header, align = 'C', ln=True)
-    pdf.ln(4)
+    pdf.ln(8)
     pdf.set_text_color(235, 9, 16)
     pdf.set_font("Arial", "B", size=24)
     pdf.cell(200, 7, txt=nome_corso_da_stampare, align = 'C', ln=True)
-    pdf.set_text_color(255,250,250)
-    pdf.ln(8)
+    pdf.set_text_color(36, 35, 35)
+    pdf.ln(6)
 
 # Stampa testo in piu righe
 def stampa_multicell(pdf, testo):
     pdf.add_font('DejaVu', '', './DejaVuSansCondensed.ttf', uni=True)
-    pdf.set_font('DejaVu', '', 9)
+    pdf.set_font('DejaVu', '', 8)
     pdf.set_margins(10, 10, 10)
     for riga in testo.splitlines():
         pdf.multi_cell(0, 5, riga)
+        # Stampa testo in piu righe
+        
+def stampa_multicell_grande(pdf, testo):
+    pdf.add_font('DejaVu', '', './DejaVuSansCondensed.ttf', uni=True)
+    pdf.set_font('DejaVu', '', 10)
+    pdf.set_margins(10, 10, 10)
+    for riga in testo.splitlines():
+        pdf.multi_cell(0, 5, riga)
+
 
 # stampa paragrafo generica con header char size = 12
 def stampa_paragrafo(pdf,header_str,text_string):
@@ -112,8 +143,10 @@ def stampa_paragrafo_secondario(pdf, header_str, text_string):
 
 # STAMPA LA TABELLA PER LE FIRME DI ACCETTAZIONE DEI VARI ENTI
 def stampa_firme_accettazione(pdf, data_emissione):
-    
     # Stampa Luogo e Data
+    pdf.ln(2)
+    disegna_linea(pdf)
+    filigrana(pdf)
     pdf.set_margins(10, 10, 10)
     pdf.set_font('Arial', 'B', 12)
     pdf.cell(60, 10, 'Luogo e Data', 0, 0, 'C')
@@ -121,13 +154,43 @@ def stampa_firme_accettazione(pdf, data_emissione):
     txt_date = 'Terni, lì ' + data_emissione
     pdf.text(25, pdf.get_y()+ 12, txt=txt_date)
 
-    # Stampa Ente Formativo : OBM s.r.l.
+    # Stampa Ente Formativo : 
     pdf.set_font('Arial', 'B', 12)
-    pdf.cell(60, 10, 'Il Docente', 0, 0, 'C')
+    pdf.cell(60, 10, ' ', 0, 0, 'C')
 
     # Stampa Ente Certificatore
+    pdf.cell(60, 10, 'Il Docente', 0, 1, 'C')
+    
+    # Ottieni la posizione y corrente per posizionare la firma sotto
+    y_firma = pdf.get_y()  # aggiungi qualche mm di spazio
+    larghezza_pagina = pdf.w
+    margine_dx = pdf.r_margin
+    # Calcola la posizione x della firma, allineata a destra come il testo
+    nome_formatore = info['ente_formativo']
+    img_w = 39  # larghezza immagine
+    x_firma = larghezza_pagina - margine_dx - img_w - 15
+    
+    # Stampa la firma con coordinate dinamiche
+    stampa_firma(pdf, nome_formatore, x_firma, y_firma)
+    
+    pdf.ln(15)
+    pdf.cell(60, 10, ' ', 0, 0, 'C')
+    
+    pdf.cell(60, 10, ' ', 0, 0, 'C')
     pdf.cell(60, 10, 'Il Direttore del Corso', 0, 1, 'C')
 
+def stampa_firma(pdf, nome_formatore="", x=None, y=None):
+    if not nome_formatore:
+        return
+    image_path = "firma_" + nome_formatore + ".png"
+    img_w = 39
+    img_h = 14
+    # Se x o y non sono specificati, usa valori di default
+    if x is None:
+        x = (pdf.w - img_w) / 2
+    if y is None:
+        y = pdf.get_y()
+    pdf.image(image_path, x=x, y=y, w=img_w, h=img_h)
 
 # Funzione che salva il pdf e ci da in out i percorsi in cui sono stati salvati
 def salvapdf(pdf, codice_fiscale, info, corso_id):
@@ -162,6 +225,106 @@ def convert_minutes_to_hours(minutes):
     hours = int(minutes // 60)
     return f"{hours}"
 
+def stampa_riquadri(pdf):
+    # DRAWING - Cornice + L agli angoli
+    pdf.set_draw_color(200, 200, 200)
+    w, h = pdf.w, pdf.h
+    margin = 5
+    # Rettangolo sottile interno
+    thin_border = 0.5
+    pdf.set_line_width(thin_border)
+    pdf.rect(margin, margin, w - 2*margin, h - 2*margin)
+
+    # Linee spesse "L"
+    thick_line = 4
+    length = 30
+    pdf.set_draw_color(36, 35, 35)
+    pdf.set_line_width(thick_line)
+
+    # Angolo in basso a sinistra
+    pdf.line(margin, h - margin, margin + length, h - margin)      # orizzontale
+    pdf.line(margin, h - margin, margin, h - margin - length)      # verticale
+    
+    # L Interna - Config
+    gap = 3   # margine interno tra le L
+    thick_line = 3
+    pdf.set_line_width(thick_line)
+    pdf.set_draw_color(235, 9, 16)
+    # Seconda L interna (ulteriore spostamento di gap)
+    pdf.line(margin + 2*gap, h - margin - 2*gap, margin + length - 2*gap, h - margin - 2*gap)
+    pdf.line(margin + 2*gap, h - margin - 2*gap, margin + 2*gap, h - margin - length + 2*gap)
+    
+    # Angolo in alto a destra
+    pdf.set_draw_color(36, 35, 35)
+    thick_line = 4
+    pdf.set_line_width(thick_line)
+    pdf.line(w - margin, margin, w - margin - length, margin)       # orizzontale verso sinistra
+    pdf.line(w - margin, margin, w - margin, margin + length)       # verticale verso il basso  
+    
+    # L Interna - Config
+    gap = 3   # margine interno tra le L
+    thick_line = 3
+    pdf.set_line_width(thick_line)
+    pdf.set_draw_color(235, 9, 16)
+    # coordinate spostate verso l'interno di 2*gap
+    x_start = w - margin - 2*gap
+    y_start = margin + 2*gap
+
+    # lunghezza ridotta della L interna, ad esempio length - 3*gap
+    reduced_length = length - 4*gap
+
+    pdf.line(x_start, y_start, x_start - reduced_length, y_start)       # orizzontale più corta verso sinistra
+    pdf.line(x_start, y_start, x_start, y_start + reduced_length)       # verticale più corta verso il basso
+    # Colori e spessore
+    pdf.set_draw_color(235, 9, 16)
+    pdf.set_fill_color(235, 9, 16)
+    pdf.set_line_width(1)
+
+    margin = 3
+    triangle_size = 20
+    w, h = pdf.w, pdf.h
+
+    # Triangolo in basso a destra
+    points_br = [
+        w - margin, h - margin,
+        w - margin - triangle_size, h - margin,
+        w - margin, h - margin - triangle_size
+    ]
+    polygon(pdf, points_br, style='F')
+
+    # Triangolo in alto a sinistra
+    points_tl = [
+        margin, margin,
+        margin + triangle_size, margin,
+        margin, margin + triangle_size
+    ]
+    polygon(pdf, points_tl, style='F')
+    
+def polygon(pdf, points, style='D'):
+    h = pdf.h
+    k = pdf.k
+    op = {'F': 'f', 'FD': 'b', 'DF': 'b'}.get(style, 's')
+    points_str = ''
+    for i in range(0, len(points), 2):
+        x = points[i] * k
+        y = (h - points[i+1]) * k
+        if i == 0:
+            points_str += f'{x:.2f} {y:.2f} m '
+        else:
+            points_str += f'{x:.2f} {y:.2f} l '
+    pdf._out(points_str + op)
+
+def filigrana(pdf):
+    # dimensioni pagina
+    page_w = pdf.w
+    # dimensioni immagine da usare (puoi personalizzare)
+    img_w = 30  # esempio: 100 mm di larghezza
+    img_h = 30  # esempio: 100 mm di altezza
+    # calcolo coordinate per centrare
+    x = ((page_w - img_w) / 2)
+    y = y = pdf.get_y()
+    pdf.image("logo_square_autentica.png", x=x, y=y, w=img_w, h=img_h)
+    
 # Funzione per creare PDF per ogni codice fiscale
 def crea_pdf(codice_fiscale, info):
     for corso in info['corsi']:
@@ -170,14 +333,8 @@ def crea_pdf(codice_fiscale, info):
         # dopo la modifica del 12/07/2024 ad ogni funzione va passata la variabile PDF
         # ----------------------------------------------------------------------------------------------------------------
         pdf = FPDF()
-        pdf.add_page()        
-        # Imposta colore di riempimento RGB (esempio: azzurro chiaro)
-        pdf.set_fill_color(36, 35, 35)
-        pdf.set_text_color(255,250,250)
-        
-        # Disegna un rettangolo che copre tutta la pagina
-        pdf.rect(0, 0, pdf.w, pdf.h, 'F')
-        # immagine ANPAL/UE in intestazione e stampa "ALLEGATO 7 - ..." subito sotto l'immagine
+        pdf.add_page()
+        # immagine AUTHENTICA in intestazione e stampa "attestato di formazione"subito sotto l'immagine
         image_path = './intestazione.png'
         testo_header_1 = "ATTESTATO di FORMAZIONE"
         stampa_intestazione_pdf(pdf, image_path, testo_header_1)
@@ -235,6 +392,9 @@ def crea_pdf(codice_fiscale, info):
         stampa_denominazione_percorso_sviluppo(pdf, dettagli_percorso_formativo[1]) # spaziatura verticale: pdf.ln(5)
         
         # Stampa CONTENUTI DEL CORSO
+        paragrafo_ai_sensi = "(ai sensi dell'Art.37 del D. Lgs. n° 81/08 e s.m.i. e secondo l'Accordo Stato Regioni e Provincie Autonome di Trento e Bolzano del 21.12.2011)"
+        stampa_multicell_grande(pdf, paragrafo_ai_sensi)
+        pdf.ln(2)
         # STAMPA [4] = perco_durata
         header_str = f"della durata complessiva di {dettagli_percorso_formativo[4]} ore, tenutosi in modalità FAD, con i seguenti contenuti:"
         # STAMPA [2] = perco_obiettivo
@@ -243,7 +403,7 @@ def crea_pdf(codice_fiscale, info):
         stampa_paragrafo(pdf, header_str, text_string)
         #########################################################################################################
         
-        pdf.ln(20)
+        pdf.ln(12)
         
         # Stampa diciture per firme accettazione
 
@@ -253,9 +413,12 @@ def crea_pdf(codice_fiscale, info):
 
         stampa_firme_accettazione(pdf, data_certificazione)
         #########################################################################################################
+        # Stampa riquadri e cornice
+        stampa_riquadri(pdf)
         
-        corso = dettagli_percorso_formativo[0]
         # funzione di salvataggio del PDF
+        # id_corso per distinzione nome files
+        corso = dettagli_percorso_formativo[0]
         salvapdf(pdf, codice_fiscale, info, corso) 
 
 # ----------------------------------------------------------------------------------------------------------------    
